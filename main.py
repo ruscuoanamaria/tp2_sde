01803d759323b0fee1a956c6edbd856063b2ec13
Ruscu Oana-Maria <101173968+ruscuoanamaria@users.noreply.github.com>
2022-03-29 11:53:55 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+Ex 3 
+
 def on_forever():
-    led.plot(3, 2)
-    basic.pause(100)
-    led.unplot(3, 2)
+    if input.button_is_pressed(Button.A):
+        basic.show_icon(IconNames.HAPPY)
+    else:
+        if input.button_is_pressed(Button.B):
+            basic.show_icon(IconNames.SAD)
 basic.forever(on_forever)
+
+
+ex 4 
+
+_var = 0
+
+def on_forever():
+    global _var
+    _var = 0
+    while _var < 9 and _var / 2 != 0:
+        var1 = 0
+        _var = var1
+        basic.show_number(var1)
+basic.forever(on_forever)
+
+
+Ex 5 
+_var = 0
+for index in range(9):
+    if _var % 2 == 1:
+        basic.show_number(_var)
+    _var += 1
